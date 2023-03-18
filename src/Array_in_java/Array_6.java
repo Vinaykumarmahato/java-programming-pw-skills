08f82225d42db0f9731ed1517c83e342bd8adb02
vinaykumar860964 <vinaykumar860964@gmail.com>
2023-03-18 23:06:22 +0530

    Array 6 has modified

--- src/Array_in_java/Array_6.java
+++ src/Array_in_java/Array_6.java
@@ -1,7 +1,16 @@
 package Array_in_java;
 
-public class Array_6 {
-	int arr[];
+public class Array_6{
 	
+	public static void main(String args[]) {
+			int arr[]= {
+			
+					544,454,45454,4,4544
+		};
+			for(int i=0;i<arr.length;i++) {
+				System.out.println(arr[i]);
+			}
 
-}
+		}
+
+	}
