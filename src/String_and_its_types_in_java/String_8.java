04e6f8be6e43f8ac952992bcf7e87aa6df130c10
vinaykumar860964 <vinaykumar860964@gmail.com>
2023-04-02 21:53:25 +0530

    String 8 Added

---
+++ src/String_and_its_types_in_java/String_8.java
@@ -0,0 +1,13 @@
+package String_and_its_types_in_java;
+
+public class String_8 {
+
+	public static void main(String[] args) {
+     String name1="vinay kumar";
+     String name2="VINAY KUMAR";
+     System.out.println(name1==name2);// true
+     
+     
+	}
+
+}
